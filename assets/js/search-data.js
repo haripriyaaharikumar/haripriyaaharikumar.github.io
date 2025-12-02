0b1dab88a1dc822136723f83539f0f1849e05152
haripriyaaharikumar <94691768+haripriyaaharikumar@users.noreply.github.com>
2025-12-02 17:46:07 +0000

    Deploying to gh-pages from @ haripriyaaharikumar/haripriyaaharikumar.github.io@ee9b3be3e7df61957fbd503a91d0f70bc5ad9bde 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -513,28 +513,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%61%72%69%70%72%69%79%61%61%68%61%72%69%6B%75%6D%61%72@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/haripriyaaharikumar# your GitHub user name", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/al-folio/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/haripriya-harikumar# your LinkedIn user name", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=50ErN80AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
